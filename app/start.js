f25edeb7c51f7cca8ea950e30941a22f6a7d54dc
Samuel Rowe <samuelrowe1999@gmail.com>
2020-09-24 15:37:57 +0530

    Integrated dotenv

--- app/start.js
+++ app/start.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 
-const port = process.env.PORT || 3001;
+const port = process.env.SERVER_PORT;
+const databaseUrl = process.env.DATABASE_URL;
 
-mongoose.connect("mongodb://localhost/hubble_subscriptions", {
+mongoose.connect(databaseUrl, {
     useNewUrlParser: true,
 });
 mongoose.connection.on("error", console.error.bind(console, "[error] "));
