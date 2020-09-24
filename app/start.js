fc6f12d34695ac467b0cd5dc2e246f4d8e3d02ec
Samuel Rowe <samuelrowe1999@gmail.com>
2020-09-24 13:46:11 +0000

    Updated the server to support HTTPS.

--- app/start.js
+++ app/start.js
@@ -1,8 +1,17 @@
 const mongoose = require("mongoose");
 const app = require("./app");
+const https = require("https");
+const fs = require("fs");
 
 const port = process.env.SERVER_PORT;
 const databaseUrl = process.env.DATABASE_URL;
+
+let serverOptions = {};
+if (process.env.TARGET === "deploy") {
+    serverOptions.key = fs.readFileSync(process.env.SSL_PRIVATE_KEY_PATH);
+    serverOptions.cert = fs.readFileSync(process.env.SSL_CERTIFICATE_PATH);
+    serverOptions.ca = fs.readFileSync(process.env.SSL_CHAIN_PATH);
+}
 
 mongoose.connect(databaseUrl, {
     useNewUrlParser: true,
@@ -10,7 +19,7 @@ mongoose.connect(databaseUrl, {
 mongoose.connection.on("error", console.error.bind(console, "[error] "));
 mongoose.connection.once("open", function () {
     console.log("Database connection successfully established.");
-    app.listen(port, function () {
+    https.createServer(serverOptions, app).listen(port, () => {
         console.log("You can access the server at http://localhost:" + port);
     });
 });
