62d3d0935dc215a74e8c687ae43ab2f5ecde35fa
Samuel Rowe <samuelrowe1999@gmail.com>
2020-09-27 14:25:38 +0530

    Fixed a few subtle bugs.

--- app/start.js
+++ app/start.js
@@ -6,11 +6,11 @@ const fs = require("fs");
 const port = process.env.SERVER_PORT;
 const databaseUrl = process.env.DATABASE_URL;
 
-let serverOptions = {};
+let options = {};
 if (process.env.TARGET === "deploy") {
-    serverOptions.key = fs.readFileSync(process.env.SSL_PRIVATE_KEY_PATH);
-    serverOptions.cert = fs.readFileSync(process.env.SSL_CERTIFICATE_PATH);
-    serverOptions.ca = fs.readFileSync(process.env.SSL_CHAIN_PATH);
+    options.key = fs.readFileSync(process.env.SSL_PRIVATE_KEY_PATH);
+    options.cert = fs.readFileSync(process.env.SSL_CERTIFICATE_PATH);
+    options.ca = fs.readFileSync(process.env.SSL_CHAIN_PATH);
 }
 
 mongoose.connect(databaseUrl, {
@@ -19,7 +19,7 @@ mongoose.connect(databaseUrl, {
 mongoose.connection.on("error", console.error.bind(console, "[error] "));
 mongoose.connection.once("open", function () {
     console.log("Database connection successfully established.");
-    https.createServer(serverOptions, app).listen(port, () => {
+    https.createServer(options, app).listen(port, () => {
         console.log("You can access the server at http://localhost:" + port);
     });
 });
