983d90daf3481bfd3bce5649ee1f640362bfdacd
Samuel Rowe <samuelrowe1999@gmail.com>
2020-09-29 11:22:39 +0530

    A HTTP server is now started during development.
    For production, we require the HTTPS protocol. However, we need HTTP for development.
    Therefore, the code was extended to dynamically use either HTTPS or HTTP based on `.env`.

--- app/start.js
+++ app/start.js
@@ -1,13 +1,17 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 const https = require("https");
+const http = require("http");
 const fs = require("fs");
 
 const port = process.env.SERVER_PORT;
 const databaseUrl = process.env.DATABASE_URL;
 
+/* Start a HTTPS server only at production. */
+let createServer = http.createServer;
 let options = {};
 if (process.env.TARGET === "deploy") {
+    createServer = https.createServer;
     options.key = fs.readFileSync(process.env.SSL_PRIVATE_KEY_PATH);
     options.cert = fs.readFileSync(process.env.SSL_CERTIFICATE_PATH);
     options.ca = fs.readFileSync(process.env.SSL_CHAIN_PATH);
@@ -19,7 +23,7 @@ mongoose.connect(databaseUrl, {
 mongoose.connection.on("error", console.error.bind(console, "[error] "));
 mongoose.connection.once("open", function () {
     console.log("Database connection successfully established.");
-    https.createServer(options, app).listen(port, () => {
+    createServer(options, app).listen(port, () => {
         console.log("You can access the server at http://localhost:" + port);
     });
 });
